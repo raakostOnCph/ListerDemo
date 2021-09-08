5fd87855d57293702fed7166bacb5f07edf40860
Nikolaj B Hemmeshøj <nbh@cphbusiness.dk>
2021-09-08 09:11:19 +0200

    har listen og knuderne

---
+++ src/com/company/Node.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Node
+{
+
+    String data;
+
+    Node n = null;
+    Node p = null;
+
+    public Node(String data)
+    {
+        this.data = data;
+    }
+}
